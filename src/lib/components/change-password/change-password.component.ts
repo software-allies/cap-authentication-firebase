a8f732409734e9c05d6dfbdf91b86d41ebb56c5c
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-30 11:18:17 -0600

    CAP-309 src/lib/components/change-password/change-password.component.ts implement the UI and the functions to be used for the change-password component.

---
+++ src/lib/components/change-password/change-password.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthenticationService } from '../../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: "cap-change-password-firebase",
+  template: `
+  <div class="container register-form">
+  <div class="form">
+    <div class="header">
+        <p>Forgot Password</p>
+    </div>
+
+    <form [formGroup]="changeform" (ngSubmit)="forgorPassword()">
+      <div class="form-content">
+        <div class="form-group d-flex justify-content-center row">
+          <label *ngIf="!emailSend" class="col-12  text-center col-form-label">
+            Enter your email address and we will send you a link to reset your password.
+          </label>
+          <label *ngIf="emailSend" class="col-12  text-center col-form-label">
+            An e-mail was sent to your email address that you provided, there you can change your password.
+          </label>
+          <label *ngIf="errorEmailSend" class="col-12 text-danger text-center col-form-label">
+            an error occurred with the server when checking your email, try again later
+        </label>
+          <div class="col-4">
+            <input  *ngIf="!emailSend"
+                    [ngClass]="{
+                      'invalidField':validatedForm
+                    }"
+                    class="form-control"
+                    type="email"
+                    placeholder="Email address"
+                    formControlName="email">
+            <small id="passwordHelpBlock" class="form-text text-center text-muted">
+            </small>
+            <button *ngIf="!emailSend" type="submit" class="btnSubmit">Send Email</button>
+            <button *ngIf="emailSend" type="button"  (click)="goToHome()" class="btnSubmit">Go to Home</button>
+          </div>
+        </div>
+      </div>
+    </form>
+
+  </div>
+</div>
+  `,
+  styles: [`
+  .header
+  {
+    text-align: center;
+    height: 50px;
+    background: black;
+    color: #fff;
+    font-weight: bold;
+    line-height: 50px;
+  }
+  .form-content
+  {
+    min-height: 150px;
+    padding: 5%;
+    border: 1px solid #ced4da;
+    margin-bottom: 2%;
+  }
+  .form-control{
+    border-radius:1.5rem;
+  }
+  .btnSubmit
+  {
+    border:none;
+    border-radius:1.5rem;
+    padding: 1%;
+    width: 100%;
+    cursor: pointer;
+    background: black;
+    color: #fff;
+  }
+  .invalidField{
+    border-color:#dc3545;
+  }
+  button {
+    outline: none;
+  }
+  `],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class AuthChangePasswordComponent implements OnInit{
+
+  changeform: FormGroup;
+  emailSend: boolean;
+  errorEmailSend: boolean;
+  validatedForm: boolean;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {
+    this.emailSend = false;
+    this.errorEmailSend = false;
+    this.validatedForm = false;
+  }
+
+  ngOnInit() {
+    this.changeform = new FormGroup({
+      'email': new FormControl('', [Validators.required]),
+    });
+  }
+
+  forgorPassword() {
+    if (this.changeform.valid) {
+      this.authenticationService.changePassword(this.changeform.value).then((user: any) => {
+        this.emailSend = true;
+      }).catch(error => this.errorEmailSend = true);
+    } else {
+      this.validatedForm = true;
+    }
+  }
+
+  goToHome() {
+    this.router.navigate(['/']);
+  }
+}
