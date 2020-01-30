6533bfe38ac1b24b07289f0a6098ff0904e5eda4
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-30 11:22:52 -0600

    CAP-309 src/lib/services/authentication.service.ts Implement all the functions that are used in the module so that they are called from the service and shared for all components.

---
+++ src/lib/services/authentication.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, Inject, PLATFORM_ID} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable()
+export class AuthenticationService {
+
+  private user: Observable<firebase.User | null>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    @Inject(PLATFORM_ID) private platformId,
+  ) {
+    this.user = this.afAuth.authState;
+  }
+
+  get authenticated(): boolean {
+    return this.user != null;
+  }
+
+  get currentUser(): Observable<firebase.User | null> {
+    return this.user;
+  }
+
+  createUser(user: any): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+
+  loginUser(user: any): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+
+  signOut(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      if (localStorage.getItem('User')) {
+        localStorage.removeItem('User');
+      }
+    }
+    return this.afAuth.auth.signOut();
+  }
+
+  saveCurrentUSer(user: {}) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('User', JSON.stringify(user));
+    }
+  }
+
+  authWithFacebook(): Promise<firebase.auth.UserCredential> {
+    const provider: firebase.auth.FacebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday');
+    return this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  authWithGoogle(): Promise<firebase.auth.UserCredential> {
+    const provider: firebase.auth.GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  updateProfile = (user: any): Promise<void> =>
+    this.afAuth.auth.currentUser
+    ? this.afAuth.auth.currentUser.updateProfile({
+      displayName: user.displayName
+    })
+    : Promise.resolve()
+
+  changePassword(user: any): Promise<void> {
+    return this.afAuth.auth.sendPasswordResetEmail(user.email);
+  }
+
+  verifyEmail(): Promise<void> {
+    return this.afAuth.auth.currentUser.sendEmailVerification();
+  }
+
+}
