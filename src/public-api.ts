4ca534d6b94fbf7f1de82c107e28deb4d118f7b4
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-01-30 12:06:17 -0600

    CAP-309 src/public-api.ts Export all components, module services to be able to use outside the module.

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,11 @@
 /*
  * Public API Surface of cap-authentication-firebase
  */
+
+export * from './lib/authentication.module';
+export * from './lib/services/authentication.service';
+export * from './lib/components/change-password/change-password.component';
+export * from './lib/components/log-out/log-out.component';
+export * from './lib/components/login/login.component';
+export * from './lib/components/profile/profile.component';
+export * from './lib/components/register/register.component';
