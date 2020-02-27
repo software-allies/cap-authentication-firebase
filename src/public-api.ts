8f7fc31d34ae33f01cde8ba7b84852fe9464cb80
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-02-27 10:23:37 -0600

    CAP-310 src/public-api.ts export the new services that was created.

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,6 +4,8 @@
 
 export * from './lib/authentication.module';
 export * from './lib/services/authentication.service';
+export * from './lib/services/guard.service';
+export * from './lib/services/state.service';
 export * from './lib/components/change-password/change-password.component';
 export * from './lib/components/log-out/log-out.component';
 export * from './lib/components/login/login.component';
