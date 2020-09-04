bd28a07e79cae1502f2f1f9cb1c63bf77785945d
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-09-04 11:01:52 -0500

    TM-172 src/lib/interfaces/authentication.interface.ts create a data model (interface) to cover the variables that we return in the promises.

---
+++ src/lib/interfaces/authentication.interface.ts
@@ -0,0 +1,34 @@
+export interface Register {
+  userData: UserData;
+  response: any;
+}
+
+export interface RegisterJWT {
+  authTime: Date;
+  claims: Claims;
+  expirationTime: Date;
+  issuedAtTime: Date;
+  signInProvider: string;
+  token: string;
+}
+
+export interface Claims {
+  aud: string;
+  auth_time: number;
+  email: string;
+  email_verified: boolean;
+  exp: number;
+  firebase: any;
+  iat: any;
+  iss: string;
+  sub: string;
+  user_id: string;
+}
+
+export interface UserData {
+  email?: string;
+  password?: string;
+  firstName: string;
+  lastName?: string;
+  company?: string;
+}
