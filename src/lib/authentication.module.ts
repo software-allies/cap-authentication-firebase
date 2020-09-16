e2e89cd3a6b4b183788df8d828d788ce47c5ae65
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2020-09-16 13:47:07 -0500

    TM-154 src/lib/authentication.module.ts Import the AuthForgotComponent component into the main module.

--- src/lib/authentication.module.ts
+++ src/lib/authentication.module.ts
@@ -8,8 +8,10 @@ import { AuthRegisterComponent } from './components/register/register.component'
 import { AuthLoginComponent } from './components/login/login.component';
 import { AuthProfileComponent } from './components/profile/profile.component';
 import { AuthChangePasswordComponent } from './components/change-password/change-password.component';
+import { AuthForgotComponent } from './components/forgot/forgot.component';
+
 import { logOutComponent } from './components/log-out/log-out.component';
-import { AuthVerifyComponent } from './components/verify/veryfy.component';
+import { AuthVerifyComponent } from './components/verify/verify.component';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { StateService } from './services/state.service';
 import { GuardService } from './services/guard.service';
@@ -26,7 +28,8 @@ import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
     AuthChangePasswordComponent,
     AuthProfileComponent,
     AuthVerifyComponent,
-    logOutComponent
+    logOutComponent,
+    AuthForgotComponent
   ],
   imports: [
     HttpClientModule,
@@ -43,7 +46,8 @@ import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
     AuthChangePasswordComponent,
     AuthProfileComponent,
     AuthVerifyComponent,
-    logOutComponent
+    logOutComponent,
+    AuthForgotComponent
   ],
   providers: [
     AuthenticationService,
